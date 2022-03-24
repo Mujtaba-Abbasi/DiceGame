9b621f6e5cbfda5d8581678b3b3e38af3593bd21
Ghulam Mujtaba <100614466+Mujtaba-Abbasi@users.noreply.github.com>
2022-03-24 17:51:01 +0500

    Update index.js
    Pictures weren't showing up!

--- index.js
+++ index.js
@@ -8,33 +8,33 @@ if (randomNumber === 1) {
     document.querySelector(".img1").setAttribute("src", "/images/dice1.png");
 }
 else if (randomNumber === 2) {
-    document.querySelector(".img1").setAttribute("src", "/images/dice2.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
 }
 else if (randomNumber === 3) {
-    document.querySelector(".img1").setAttribute("src", "/images/dice3.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
 }
 else if (randomNumber === 4) {
-    document.querySelector(".img1").setAttribute("src", "/images/dice4.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
 }
 else if (randomNumber === 5) {
-    document.querySelector(".img1").setAttribute("src", "/images/dice5.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
 }
 
 
 if (randomNumber1 === 1) {
-    document.querySelector(".img2").setAttribute("src", "/images/dice1.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
 }
 else if (randomNumber1 === 2) {
-    document.querySelector(".img2").setAttribute("src", "/images/dice2.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
 }
 else if (randomNumber1 === 3) {
-    document.querySelector(".img2").setAttribute("src", "/images/dice3.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
 }
 else if (randomNumber1 === 4) {
-    document.querySelector(".img2").setAttribute("src", "/images/dice4.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
 }
 else if (randomNumber1 === 5) {
-    document.querySelector(".img2").setAttribute("src", "/images/dice5.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
 }
 
 
